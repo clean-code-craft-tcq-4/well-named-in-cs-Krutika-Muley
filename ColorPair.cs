c620b9d69507ada2e419503cd40b7665e22e6814
Krutika-Muley <91215900+Krutika-Muley@users.noreply.github.com>
2022-06-14 23:41:43 +0530

    Create ColorPair.cs

---
+++ ColorPair.cs
@@ -0,0 +1,12 @@
+        /// <summary>
+        /// data type defined to hold the two colors of clor pair
+        /// </summary>
+        internal class ColorPair
+        {
+            internal Color majorColor;
+            internal Color minorColor;
+            public override string ToString()
+            {
+                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+            }
+        }
